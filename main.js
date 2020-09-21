b74adf4d4afa62b10639213ff2f2f9e2549f3e89
MartyLucas45 <marty45@hotmail.fr>
2020-09-21 09:52:51 +0200

    Mise à jour quêtes

---
+++ main.js
@@ -0,0 +1,42 @@
+console.log("Hello, World");
+console.info ("c'est chaud");
+console.error ("La c'est tendu0")
+console.warn ("il n'y a plus de machine arrière")
+// Ici c'est la ligne de commentaire
+
+/* la on peux les
+super posé */
+
+console.log (1 + 1);
+let myname = "Marty";
+console.log(myname);
+
+myname = "MakFly";
+console.log(myname);
+
+
+console.log("Départ ici");
+
+const name = "David";
+const name2 = "Bruno";
+const description = "Hi, I'm David Bruno from SF, I like to cook and meet new people.";
+
+const img = "http://www.go.com/davif.png";
+
+const city = "San Francisco";
+const abstract = "CA";
+
+let people_follower = 109;
+let people_following = 1500;
+people_following ++;
+
+
+let actu_DATE = new Date();
+
+
+ /*
+
+   🏁 David is following one more account, increment the total of following account
+
+ */
+
